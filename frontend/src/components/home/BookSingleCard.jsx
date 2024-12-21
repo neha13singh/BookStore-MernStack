93a7d1cdd23087ceecb32fb91b79f3f235469aa2
neha singh <neha786.india@gmail.com>
2024-12-21 11:43:16 +0530

    book card look adjusted

--- frontend/src/components/home/BookSingleCard.jsx
+++ frontend/src/components/home/BookSingleCard.jsx
@@ -16,7 +16,7 @@ const BookSingleCard = ({ book }) => {
       <h2 className='absolute top-1 right-2 px-4 py-1 bg-red-300 rounded-lg'>
         {book.publishYear}
       </h2>
-      <h4 className='my-2 text-gray-500'>{book._id}</h4>
+      {/* <h4 className='my-2 text-gray-500'>{book._id}</h4> */}
       <div className='flex justify-start items-center gap-x-2'>
         <PiBookOpenTextLight className='text-red-300 text-2xl' />
         <h2 className='my-1'>{book.title}</h2>
@@ -25,10 +25,10 @@ const BookSingleCard = ({ book }) => {
         <BiUserCircle className='text-red-300 text-2xl' />
         <h2 className='my-1'>{book.author}</h2>
       </div>
-      <div className='my-2'>
+      {/* <div className='my-2'>
         <h4 className='text-gray-500'>Summary</h4>
         <p>{book.summary||"no data"}</p>
-      </div>
+      </div> */}
       <div className='flex justify-between items-center gap-x-2 mt-4 p-4'>
         <Link to={`/books/view/${book._id}`}>
           <BiShow className='text-2xl text-blue-800 hover:text-blue-600' />
